69bdb0f4314b7da8349b423f07d1448e6ca33fee
AshishManer <108808771+ash44444@users.noreply.github.com>
2024-04-15 21:24:14 +0530

    Update server.js
    RAEGRETGR

--- server/server.js
+++ server/server.js
@@ -23,16 +23,14 @@ io.on('connection', (socket) => {
   });
 });
 const __dirname1=path.resolve();
-if(process.env.NODE_ENV===production){
+
   app.use(express.static(path.join(__dirname1,'/frontend/build')))
 
   app.get('*',(req,res)=> {
     res.sendFile(path.resolve(__dirname1,'frontend','build','index.html'))
   })
-}
-else{
 
-}
+
 
 
 
